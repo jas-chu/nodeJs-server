ac75e20cde90096da51abdd7852b7feecdafdbab
Jasmina <jasminasf@gmail.com>
2016-12-01 15:04:01 -0300

    pathname

--- http.js
+++ http.js
@@ -1,12 +1,14 @@
 var http = require("http");
 var url = require("url");
+var fs = require("fs");
 
 var server = http.createServer();
 
 server.on("request", function(req, res){
 	var urlData = url.parse(req.url, true);
-	urlData.query['id'];
-	res.end(JSON.stringify(urlData));
+	var pathname = "public" + urlData.pathname;
+	urlData.query['path'];
+	res.end(JSON.stringify(pathname));
 
 	//var f=new Date();
 	//cad=f.getHours()+":"+f.getMinutes(); 
@@ -25,6 +27,9 @@ function fileExists(path) {
     return false;
   }
 }
+
+//fs.exists(path, callback)
+//fs.readFile(path, callback)
 
 //lee un pathname desde la urlData busca un fichero con esa ruta dentro de ./public
 //si existe, lo sirve

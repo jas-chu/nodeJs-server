06dfc8c36ae62983d0f4354f2ae6d3965089ea7e
Jasmina <jasminasf@gmail.com>
2016-12-01 14:34:39 -0300

    http process.env.PORT 3000

--- http.js
+++ http.js
@@ -1,10 +1,31 @@
 var http = require("http");
+var url = require("url");
+
 var server = http.createServer();
 
 server.on("request", function(req, res){
-	var f=new Date();
-	cad=f.getHours()+":"+f.getMinutes(); 
-	res.end(cad);
+	var urlData = url.parse(req.url, true);
+	urlData.query['id'];
+	res.end(JSON.stringify(urlData));
+
+	//var f=new Date();
+	//cad=f.getHours()+":"+f.getMinutes(); 
+	//res.end(cad);
 });
 
 server.listen(process.env.PORT || 3000);
+
+
+function fileExists(path) {
+  try {
+    if(fs.accessSync(path)) {
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+}
+
+//lee un pathname desde la urlData busca un fichero con esa ruta dentro de ./public
+//si existe, lo sirve
+//si no existe, devuelve 404.
